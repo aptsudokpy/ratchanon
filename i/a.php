38a06fc634898f1f35fc8819e884f323e22681b7
aptsudokpy <66010914154@msu.ac.th>
2026-02-10 09:41:03 +0700

    Add files via upload

---
+++ i/a.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>รัชชานนท์ พรดีมา (นิวเคลียร์)</title>
+</head>
+
+<body>
+    <h1>งาน i</h1>
+    <h1>รัชชานนท์ พรดีมา (นิวเคลียร์)</h1>
+</body>
+</html>
