37c51b19794f61191df74a807dec0d1a4945fc2c
aptsudokpy <66010914154@msu.ac.th>
2026-02-17 12:11:34 +0700

    Update connectDB.php

--- j/connectDB.php
+++ j/connectDB.php
@@ -1,9 +1,10 @@
 <?php
                 $host ="localhost";  
                 $user = "root";
-                $pwd = "";
+                $pwd = "123456789";
                 $DB = "4154db"; 
                 #เชื่อมต่อฐานข้อมูล
                 $conn = mysqli_connect($host,$user,$pwd,$DB) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
                 mysqli_query($conn, "SET NAMES utf8");
+
 ?>
